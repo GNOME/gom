113794f1c7d99f509b8d484b104c8c017d754ed5
Christian Hergert <chris@dronelabs.com>
2011-02-08 02:56:22 -0800

    Add GomAdapter gtk-docs

--- gom/gom-adapter.c
+++ gom/gom-adapter.c
@@ -31,6 +31,17 @@ gom_adapter_init (GomAdapter *adapter)
 {
 }
 
+/**
+ * gom_adapter_create:
+ * @adapter: (in): A #GomAdapter.
+ * @enumerable: (in): A #GomEnumerable.
+ * @error: (error): A location for a #GError, or %NULL.
+ *
+ * Creates a series of new #GomResource<!-- -->'s found in enumerable.
+ *
+ * Returns: %TRUE if successful; otherwise %FALSE.
+ * Side effects: None.
+ */
 gboolean
 gom_adapter_create (GomAdapter     *adapter,
                     GomEnumerable  *enumerable,
@@ -39,6 +50,17 @@ gom_adapter_create (GomAdapter     *adapter,
 	return GOM_ADAPTER_GET_CLASS(adapter)->create(adapter, enumerable, error);
 }
 
+/**
+ * gom_adapter_delete:
+ * @adapter: (in): A #GomAdapter.
+ * @collection: (in): A #GomCollection of #GomResource<!-- -->'s.
+ * @error: (in): A location for a #GError, or %NULL.
+ *
+ * Deletes a resource from the underlying data adapter.
+ *
+ * Returns: %TRUE if successful; otherwise %FALSE.
+ * Side effects: None.
+ */
 gboolean
 gom_adapter_delete (GomAdapter     *adapter,
                     GomCollection  *collection,
@@ -47,6 +69,20 @@ gom_adapter_delete (GomAdapter     *adapter,
 	return GOM_ADAPTER_GET_CLASS(adapter)->delete(adapter, collection, error);
 }
 
+/**
+ * gom_adapter_update:
+ * @adapter: (in): A #GomAdapter.
+ * @properties: (in): A set of properties to update.
+ * @values: (in): The values for @properties.
+ * @collection: (in): A collection of resources that should be updated.
+ *
+ * Updates the resources found in @collection. The properties found in
+ * @properties are updated to the values found in @values at the
+ * corresponding index.
+ *
+ * Returns: %TRUE if successful; otherwise %FALSE.
+ * Side effects: None.
+ */
 gboolean
 gom_adapter_update (GomAdapter     *adapter,
                     GomPropertySet *properties,
