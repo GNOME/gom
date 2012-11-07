2861070c8838934c36df0928e0712f4390713a55
Christian Hergert <chris@dronelabs.com>
2012-11-06 20:32:05 -0800

    build: cleanup build system a bit.
    use gnome-autogen.sh, cleanup gtk-doc stuff.

--- autogen.sh
+++ autogen.sh
@@ -1,17 +1,3 @@
 #!/bin/sh
 
-srcdir=`dirname $0`
-test -z "$srcdir" && srcdir=.
-
-test -f "configure.ac" || {
-	echo "You must run this script in the top-level gom directory"
-	exit 1
-}
-
-autoreconf -v --install || exit $?
-
-test -n "$NOCONFIGURE" ||
-(
-	"$srcdir/configure" "$@" &&
-	echo "Now type 'make' to compile gom"
-)
+PKG_NAME=gom gnome-autogen.sh $@
