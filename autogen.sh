40845acd6107b8b3f119b10a81853c2e64559da0
Emmanuele Bassi <ebassi@gnome.org>
2016-06-03 00:06:43 +0100

    build: Drop use of gnome-autogen
    It's deprecated, and it badly breaks when used by jhbuild in a
    non-srcdir build configuration.

--- autogen.sh
+++ autogen.sh
@@ -1,3 +1,47 @@
 #!/bin/sh
 
-PKG_NAME=gom gnome-autogen.sh $@
+test -n "$srcdir" || srcdir=`dirname "$0"`
+test -n "$srcdir" || srcdir=.
+
+olddir=`pwd`
+
+cd $srcdir
+
+AUTORECONF=$(which autoreconf 2>/dev/null)
+GTKDOCIZE=$(which gtkdocize 2>/dev/null)
+INTLTOOLIZE=$(which intltoolize 2>/dev/null)
+
+set -x
+
+if test -z $GTKDOCIZE; then
+	echo "*** No gtkdocize found, please install gtk-doc ***"
+	exit 1
+else
+        ${GTKDOCIZE} || exit $?
+fi
+
+if test -z $INTLTOOLIZE; then
+        echo "*** No intltoolize found, please install intltool ***"
+        exit 1
+else
+        ${INTLTOOLIZE} --force --copy --automake || exit $?
+fi
+
+if test -z $AUTORECONF; then
+        echo "*** No autoreconf found, please install autoconf ***"
+        exit 1
+else
+        ${AUTORECONF} --force --install --verbose || exit $?
+fi
+
+if test -z "$NOCONFIGURE"; then
+        if test -z "$*"; then
+                echo "*** I am going to run ./configure with no arguments - if you wish"
+                echo "*** to pass any to it, please specify them on the $0 command line."
+        fi
+fi
+
+cd "$olddir"
+test -n "$NOCONFIGURE" || "$srcdir/configure" "$@"
+
+{ set +x; } 2>/dev/null
